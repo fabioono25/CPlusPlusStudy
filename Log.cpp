0d7a4eb2283905fb4996e39675dec92e98573695
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-13 15:47:42 +0000

    First project commig

---
+++ Log.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define INTEGER int
+
+void Log(const char* message) {
+
+    INTEGER res = 1 * 3;
+    std::cout << message << res << std::endl;
+
+#if 0
+    int x = 12;
+    std::cout << "hello" << std::endl;
+#endif
+
+#include "EndBrace.h"
---
+++ Tests.cpp
@@ -0,0 +1,11 @@
+// pre-processor statement
+#include <iostream>
+
+void Log(const char* message);
+
+int main() // entry point
+{
+    Log("hi");
+    std::cout << "Hello World!" << std::endl;
+}
+
