15c7f8c0950ec013ad15474b963e21879d176563
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-15 10:08:27 +0000

    working with variables

--- Tests.cpp
+++ Tests.cpp
@@ -5,7 +5,24 @@ void Log(const char* message);
 
 int main() // entry point
 {
-    Log("hi");
-    std::cout << "Hello World!" << std::endl;
-}
+    /*Log("hi");
+    std::cout << "Hello World!" << std::endl;*/
+
+    // variables:
+    int variable = 8; // -2b ~ +2b - 4 bytes
+    unsigned int variable2 = 8;
+    
+
+    char a = 'A';
+    char b = 65;
+    std::cout << a << b << std::endl;
+
+    float f = 5.5;
+    double f1 = 5.2;
+    std::cout << f << f1 << std::endl;
+
+    std::cout << sizeof(bool) << std::endl;    //1
+    std::cout << sizeof(float) << std::endl;   //4
+    std::cout << sizeof(double) << std::endl;  //8 
+} 
 
